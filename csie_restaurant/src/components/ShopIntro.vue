af32862b77f230e412a6db2dc04353e903cc8541
Roger_NTUT_LAB1321 <asd30135931@gmail.com>
2020-11-29 05:31:02 +0800

    add shopIntro.vue

---
+++ csie_restaurant/src/components/ShopIntro.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class='container'>
+        <div class='row '>
+            <div class='col-md-4'>
+                <b-card
+                    overlay 
+                    title="SHOP"
+                    title-tag="h5"
+                    :img-src= "imgPath"
+                    img-alt="Card Image"
+                    img-width="ˇ300"
+                    img-height="150"
+                    :sub-title="shopName"
+                    sub-title-tag="h3"
+                    text-variant="white"
+                    sub-title-text-variant="white"
+                    class="text-center"
+                    style= "font-family: Lucida Console, Courier, monospace font-weight: bold;"
+                    >
+                </b-card>
+            </div>
+            <div class='col-md-8'>
+                <div class='grid-container'>
+                    <div class ='grid-item'>商品:{{commodity}}</div>
+                    <div class ='grid-item'>粉絲:{{fans}}</div>
+                    <div class ='grid-item'>評分{{rate}}</div>
+                    <div class ='grid-item'>加入時間:{{joinDate}}</div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'ShopIntro',
+    props:{
+        shopName: String,
+        imgPath: String,
+        commodity: Number,
+        fans: Number,
+        joinDate: Date,
+        rate: Number
+        
+    }
+}
+</script>
+
+<style scoped>
+
+.flex{
+    display:flex;
+    justify-content:center;
+    color:#FFFFFF;
+    flex-direction: column;
+}
+h1{
+    font-size: 44px;
+}
+
+.row{
+    margin: 1.5%;
+}
+.style1{
+    width: 100%;
+    background-color: #132328 !important
+}
+.introText{
+    padding: 3%;
+}
+.container{
+    max-width: 100%;
+    padding: 0%;
+    /* padding:0,0,0,0; */
+    /* overflow: hidden; */
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  background-color: #FDFDFD;
+  color: #492645;
+  padding: 10px;
+}
+.grid-item {
+  padding: 5% 0 5% 0 ;
+  font-size: 16px;
+  text-align: left;
+  color: #492645;
+}
+
+</style>
+
+
---
+++ csie_restaurant/src/views/Shop.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="shop">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import Search from "@/components/Search.vue";
+//https://placekitten.com/300/300
+export default {
+  name: "shop",
+  components: {
+      
+  }
+};
+</script>
+
+<style scopped>
+@media  (min-width: 800px) {
+  .container{
+    max-width:100% !important; 
+    padding: 0% !important;
+    overflow: hidden;
+  } 
+}
+
+</style>
