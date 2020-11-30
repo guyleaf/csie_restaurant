7d05ee3fcc6f3523cf97ccb9b7a5ef9dddbd0e03
guyleaf <43506966+guyleaf@users.noreply.github.com>
2020-11-30 16:46:24 +0800

    Finish the database schemas and constraints (#1)
    * Add some database schema
    
    * Add local development dockerfile
    
    * Finish some migrations
    
    Member
    Customer
    Seller
    Coupon
    Order
    
    * Convert all models in SRS doc to migration files
    
    * Add the constraints of schemas

---
+++ database/migrations/2020_11_26_095323_create_order_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_item', function (Blueprint $table) {
+            $table->foreignId("order_id")->comment("所屬訂單編號");
+            $table->foreignId("product_id")->comment("所屬商品編號");
+            $table->unsignedInteger("quantity")->default(1)->comment("訂購數量");
+            $table->string("note")->nullable()->comment("商品備註");
+            $table->primary(["order_id", "product_id"]);
+            $table->foreign("order_id")->references("id")->on("order")
+            ->onUpdate("cascade")->onDelete("cascade");
+            $table->foreign("product_id")->references("id")->on("product")
+            ->onUpdate("cascade")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_item');
+    }
+}
