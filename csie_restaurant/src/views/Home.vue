14b9052fbddd204a758e853d6f170316317582f1
failmaron <failmaron0307@gmail.com>
2020-12-02 02:05:00 +0800

    put search into nav bar and remove Search.vue

--- csie_restaurant/src/views/Home.vue
+++ csie_restaurant/src/views/Home.vue
@@ -2,7 +2,6 @@
   <div class="home">
     <Carousel imgPath1="https://picsum.photos/1024/480/?image=54" imgPath2='https://picsum.photos/1024/480/?image=12' imgPath3='https://picsum.photos/1024/480/?image=22'/>
     <center >
-      <Search />
       <div class="container row justify-content-center cardMargin">
         <div class="col-10">
           <b-card>
@@ -23,14 +22,12 @@
 
 <script>
 // @ is an alias to /src
-import Search from "@/components/Search.vue";
 import ShopCardGroup from "@/components/ShopCardGroup.vue";
 import Carousel from "@/components/Carousel.vue";
 import Checkbox from "@/components/Checkbox.vue";
 export default {
   name: "home",
   components: {
-    Search,
     Carousel,
     Checkbox,
     ShopCardGroup,
