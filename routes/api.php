f8d875b149a0ce607fe9d3526c1e6b291aca90db
guyleaf <43506966+guyleaf@users.noreply.github.com>
2020-12-04 18:23:17 +0800

    [Pull request] Complete basic auth function (#4)
    * Implement login, logout, refresh api
    
    * Optimize local dev environment
    
    Add auto sync function

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Api Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'auth'], function () use ($router) {
+    $router->post('login', 'AuthController@login');
+
+    $router->group(['middleware' => 'jwt.auth'], function () use ($router) {
+        $router->post('logout', 'AuthController@logout');
+        $router->post('refresh', 'AuthController@refresh');
+        $router->post('', 'AuthController@me');
+    });
+});
+?>
