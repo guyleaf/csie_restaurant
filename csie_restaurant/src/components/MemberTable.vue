456dbd1cce6a72438a2bad03a31fc416e4a6c561
Roger_NTUT_LAB1321 <asd30135931@gmail.com>
2020-12-22 14:46:23 +0800

    新增上下頁 控制按鈕enabled、disabled table 排版

--- csie_restaurant/src/components/MemberTable.vue
+++ csie_restaurant/src/components/MemberTable.vue
@@ -1,101 +1,152 @@
 <template>
   <div>
     <b-table  
-    :items="items" 
-    :fields="fields" 
-    :head-variant="headvariant"
-    :table-variant="teblevariant"
-    :striped="stripped"
-    :hover="hover"
-    :outlined="outlined"
-    :borderless="borderless"
-    >
-    <template #cell(function)>
-        <b-button size="sm" class="mr-2"> Details </b-button>
-        <b-button size="sm" class="mr-2"> Details </b-button>
-    </template>
+      :items="members" 
+      :fields="fields" 
+      :head-variant="headvariant"
+      :table-variant="teblevariant"
+      :striped="stripped"
+      :hover="hover"
+      :outlined="outlined"
+      :borderless="borderless"
+      >
+      <template #cell(function)>
+          <b-button size="sm" class="mr-2"> Details </b-button>
+          <b-button size="sm" class="mr-2"> Details </b-button>
+      </template>
     </b-table>
+    <div>
+      <button id='lstPg' @click="routeMembers('last')" disabled='disabled' >上一頁</button>
+      <button id='nxtPg' @click="routeMembers('next')">下一頁</button>
+    </div>
   </div>
+  
 </template>
 
 <script>
   export default {
     data() {
       return {
+        members:[],
+        currentNumber: 0,
+        requiredNumber: 5,
         // Note 'isActive' is left out and will not appear in the rendered table
         fields: [
           {
             key: 'id',
             label: '編號',
+            class:'tdId text-center',
+            tdClass: 'tdId'
           },
           {
             key: 'username',
             label: '客戶帳號',
+            class:'tdUserName text-center',
+            tdClass: 'tdUserName'
           },
           {
             key: 'name',
             label: '客戶暱稱',
+            class:'tdName text-center',
+            tdClass: 'tdName'
             // Variant applies to the whole column, including the header and footer
           },
           {
             key: 'email',
             label: '客戶信箱',
+            class:'tdemail text-center',
+            tdClass: 'tdemail'
             // Variant applies to the whole column, including the header and footer
           },
           {
             key: 'created_at',
             label: '註冊時間',
+            class: 'text-center'
             // Variant applies to the whole column, including the header and footer
           },
           {
             key: 'phone',
             label: '電話號碼',
+            class: 'text-center'
             // Variant applies to the whole column, including the header and footer
           },
           {
             key: 'member_status',
             label: '狀態',
+            class: 'text-center'
             // Variant applies to the whole column, including the header and footer
           },
           {
             key: 'function',
             label: '操作',
+            class: 'text-center'
             // Variant applies to the whole column, including the header and footer
           }
         ],
-        items: [
-          { isActive: true, id: 1, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 2, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 3, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 4, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 5, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 6, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 7, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 8, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 9, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 10, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 11, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 12, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 13, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 14, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 15, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 16, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 17, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 18, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 19, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-          { isActive: true, id: 20, username: 'asd3013593', name: '夏先生', email:'t107590001@gmail.com', created_at:'2000-02-30', phone:'0934426548', member_status:'O', },
-        ],
         headvariant:"dark",
-        tablevariant:'',
-        striped:false,
+        teblevariant:'',
+        stripped:false,
         hover:true,
         outlined:true,
         borderless:false,
       }
-    }
+    },
+    methods:{
+      routeMembers(direction){
+        this.members = []
+        if (direction == 'next')  {this.currentNumber += this.requiredNumber}
+        else if(direction == 'last') {this.currentNumber -= this.requiredNumber}
+        let url  = '/members?' + 'currentNumber=' + this.currentNumber.toString() + '&' + 'requiredNumber=' + this.requiredNumber.toString()
+        this.$http.get(url)
+        .then(response => {
+        this.members = [];
+        let data = response.data;
+        console.log(data)
+        for (let i=0;i<data.length;i++){ this.members.push({isActive:true, id:data[i].seller_id, username:data[i].username, name:data[i].name, email:data[i].email, 
+                                                            created_at:data[i].created_at, phone:data[i].phone, member_status:data[i].member_status})}
+        })
+        this.routeDisabled()
+      },
+      routeDisabled(){
+        let lstPgButton = document.getElementById("lstPg")
+        let nxtButton = document.getElementById('nxtPg')
+        let nextCurrent = this.currentNumber + this.requiredNumber
+        let url  = '/members?' + 'currentNumber=' + nextCurrent.toString() + '&' + 'requiredNumber=' + this.requiredNumber.toString()
+        if (this.currentNumber == 0){lstPgButton.disabled = true;}
+        else {lstPgButton.disabled = false;}
+        this.$http.get(url)
+        .then(response => {
+          console.log(response.data.length)
+          if (response.data.length == 0){nxtButton.disabled = true}
+          else (nxtButton.disabled = false)
+        })
+      }
+    },
+    mounted () {
+      let url = '/members?currentNumber=0 & requiredNumber=5'
+      this.$http.get(url)
+      .then(response => {
+        this.members = [];
+        let data = response.data;
+        console.log(response.data)
+        for (let i=0;i<data.length;i++){ this.members.push({isActive:true, id:data[i].seller_id, username:data[i].username, name:data[i].name, email:data[i].email, 
+                                                            created_at:data[i].created_at, phone:data[i].phone, member_status:data[i].member_status})}
+      })
+    },
   }
 </script>
 
 <style scoped>
-
+  ::v-deep .tdId{
+    width: 6%;
+  }
+  ::v-deep .tdUserName{
+    width: 15%;  
+  }
+  ::v-deep .tdName{
+    width: 15%;
+  }
+  ::v-deep .tdemail{
+    width: 20%;
+  }
 </style>
