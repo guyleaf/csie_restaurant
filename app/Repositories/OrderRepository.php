0863d24f1347ee5f9fbc0192127bc898c19ba572
guyleaf <43506966+guyleaf@users.noreply.github.com>
2020-12-29 04:25:41 +0800

    Fix order api and update columns

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -12,6 +12,11 @@ class OrderRepository
     protected $order;
 
     /**
+     * @var \Illuminate\Database\Query\Builder $product_image
+     */
+    protected $product_image;
+
+    /**
      * Member Repository constructor
      *
      * @return void
@@ -19,6 +24,7 @@ class OrderRepository
     public function __construct()
     {
         $this->order = DB::table('order', 'O');
+        $this->product_image = DB::table('product_image', 'PI');
     }
 
     /**
@@ -33,7 +39,7 @@ class OrderRepository
             ->join('seller as S', 'S.member_id', '=', 'O.seller_id')
             ->join('member as M', 'S.member_id','=','M.id')
             ->where('O.customer_id', '=', $id)
-            ->get(['O.id as order_id', 'M.name as seller_name', 'M.id as seller_id', 'O.order_time', 'O.stars']);
+            ->get(['O.id as order_id', 'M.name as seller_name', 'M.id as seller_id', 'O.status', 'O.order_time', 'O.stars']);
 
         return $order;
     }
@@ -48,19 +54,23 @@ class OrderRepository
     public function getOrderInfo($id, $orderId)
     {
         $order = $this->order
-            ->join('coupon as CP', 'CP.id', '=', 'O.coupon_id')
+            ->leftjoin('coupon as CP', 'CP.id', '=', 'O.coupon_id')
             ->where('O.id', '=', $orderId)
             ->where('O.customer_id', '=', $id)
-            ->get(['O.ship_time', 'O.payment_method', 'O.status', 'O.address',
+            ->get(['O.ship_time', 'O.payment_method', 'O.address',
             'O.fee', 'O.taking_method', 'O.stars', 'O.rating_time', 'O.comment',
             'CP.id as coupon_id', 'CP.type as coupon_type', 'CP.discount', 'CP.limit_money']);
 
         $items = $this->order
             ->join('order_item as I', 'I.order_id', '=', 'O.id')
             ->join('product as P', 'P.id', '=', 'I.product_id')
+            ->leftjoin('product_image as PI', function ($join) {
+                $join->on('P.id', '=', 'PI.id')
+                     ->limit(1);
+            })
             ->where('O.id', '=', $orderId)
             ->where('O.customer_id', '=', $id)
-            ->get(['P.id as product_id', 'P.name as product_name', 'I.quantity', 'I.price', 'I.note']);
+            ->get(['P.id as product_id', 'PI.image_path', 'P.name as product_name', 'I.quantity', 'I.price', 'I.note']);
 
         return collect(['order' => $order[0], 'order_items' => $items]);
     }
