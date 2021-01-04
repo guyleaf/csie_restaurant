e06b371fe1543e02192eb0f1ea9084316efc1afa
Lee <t107590039@ntut.org.tw>
2021-01-05 03:03:16 +0800

    介面小改、member操作、狀態按鈕新增

--- csie_restaurant/src/components/manage/FoodCard.vue
+++ csie_restaurant/src/components/manage/FoodCard.vue
@@ -128,6 +128,7 @@ export default {
             }
             this.image=input.files[0];
             reader.readAsDataURL(input.files[0]);
+            console.log(this.preview)
         }
         },
         parseCookie(){
