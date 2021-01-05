3543f86722546490ff9c65815f1622d0a30b2539
RonToSucceed <failmaron0307@gmail.com>
2021-01-05 18:14:45 +0800

    add CouponCardGroup.vue in for Viewer

---
+++ csie_restaurant/src/components/shop/CouponCard.vue
@@ -0,0 +1,36 @@
+<template>
+        <b-card overlay class='m-2' style="min-width:300px; height:180px">
+            <b-card-title>{{name}}</b-card-title>
+            <b-card-text >{{discount}} % off</b-card-text>
+            <b-card-text>期限:{{expire}}</b-card-text>
+                <!--b-col md='6'>
+                    <b-card-img
+                    :src="imgPath" 
+                    alt="Image"
+                    class="rounded-0">
+                    </b-card-img>
+                </b-col-->
+        </b-card>
+</template>
+
+<script>
+export default {
+    name: 'CouponCard',
+    data() {
+      return {
+      }
+    },
+    props:{
+        name: String,
+        discount: Number,
+        expire: Date
+    },
+    computed:{
+    },
+    methods:{        
+    }
+}
+</script>
+
+<style scoped>
+</style>
