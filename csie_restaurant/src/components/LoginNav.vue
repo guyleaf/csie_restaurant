836e5672d7453c462057f9d9a10608131b8c0678
Lee <t107590039@ntut.org.tw>
2021-01-06 02:36:30 +0800

    整理header

--- csie_restaurant/src/components/LoginNav.vue
+++ csie_restaurant/src/components/LoginNav.vue
@@ -3,6 +3,8 @@
     <b-button v-if="!logined" @click="showModal">Login</b-button>
     <b-dropdown v-else id="dropdown" :text="loginMsg" class="m-md-2" right style="min-width: 4rem">
       <b-dropdown-item><b-link :to="{name: 'History'}">History</b-link></b-dropdown-item>
+      <b-dropdown-item v-if="this.$store.getters['auth/user'].permission<=1"><b-link :to="{name: 'ShopManage'}">ProductsManage</b-link></b-dropdown-item>
+      <b-dropdown-item v-if="this.$store.getters['auth/user'].permission<=1"><b-link :to="{name: 'SalesReport'}">Manage</b-link></b-dropdown-item>
       <b-dropdown-item @click="logout">Logout</b-dropdown-item>
     </b-dropdown>
     <LoginForm ref="form" @close="closeModal" @success="loginSucess"/>
--- csie_restaurant/src/components/shared/MainHeader.vue
+++ csie_restaurant/src/components/shared/MainHeader.vue
@@ -13,15 +13,6 @@
                 <b-nav-item> 
                     <router-link :to="{name: 'Home'}" class="nav-link">Home</router-link>
                 </b-nav-item>
-                <b-nav-item> 
-                    <router-link :to="{name: 'Cashier'}" class="nav-link">Cashier</router-link>
-                </b-nav-item>
-                <b-nav-item> 
-                    <router-link :to="{name: 'ShopManage'}" class="nav-link">ShopManage</router-link>
-                </b-nav-item>
-                <b-nav-item>
-                    <router-link :to="{name: 'SalesReport'}" class="nav-link manage">Manage</router-link>
-                </b-nav-item>
             </b-navbar-nav>
             
             <!-- Right aligned nav items -->
