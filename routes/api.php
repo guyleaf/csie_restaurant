03ef4762f2e204e45c580504085dd0e2be8c40c1
Roger_NTUT_LAB1321 <61783498+asd3013593@users.noreply.github.com>
2021-01-11 03:57:22 +0800

    Backend features/shop (#94)
    * product api 多傳 sold_out and status & category 多傳 display_order
    
    * Add product function
    
    * Fix product query
    
    * Fix image path
    
    * Fix product function
    
    * Fix update Product
    
    * Fix addProduct function
    
    * Test Product Repo
    
    * Test addProduct function
    
    * Test addProduct
    
    * Fix addProduct function
    
    * Fix coupon discounts when type is 0
    
    * Add some product images and fix addProduct function
    
    * Add transaction limit and pessimistic lock
    
    * Update addProduct function
    
    Add product id added by seller to response
    
    * Fix addProduct function
    
    * Move some functions to another repo
    
    * 更新會員
    
    * 會員刪除
    
    * New DateTime
    
    * 新增會員
    
    * fix error
    
    * fix
    
    * fix error
    
    * Set permission
    
    * Fix permission
    
    * Fix permission
    
    * add seller
    
    * fix
    
    * fix error
    
    * test
    
    * test
    
    Co-authored-by: guyleaf <43506966+guyleaf@users.noreply.github.com>
    Co-authored-by: Lee <t107590039@ntut.org.tw>
    Co-authored-by: guyleaf <ychhua1@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -39,12 +39,10 @@ $router->group(['prefix' => 'restaurants'], function () use ($router) {
 
 $router->group(['prefix' => 'members'], function () use ($router) {
     $router->get('', 'MemberController@getMembers');
-    $router->post('/update', 'MemberController@updateMember');
-    $router->post('/delete', 'MemberController@deleteMember');
-    $router->post('/customer/add', 'MemberController@addCustomer');
-    $router->group(['prefix' => 'seller'], function () use ($router) {
-        $router->post('/add', 'MemberController@addSeller');
-    });
+    $router->post('update', 'MemberController@updateMember');
+    $router->post('delete', 'MemberController@deleteMember');
+    $router->post('customer/add', 'MemberController@addCustomer');
+    $router->post('seller/add', 'MemberController@addSeller');
 });
 
 $router->group(['prefix' => 'customer'], function () use ($router) {
