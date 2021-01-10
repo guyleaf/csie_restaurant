8d162e21fdd9c8dcbfeda53f5644ddea77c3fca3
Roger_NTUT_LAB1321 <61783498+asd3013593@users.noreply.github.com>
2021-01-11 05:06:20 +0800

    Backend features/shop (#98)
    * product api 多傳 sold_out and status & category 多傳 display_order
    
    * Add product function
    
    * Fix product query
    
    * Fix image path
    
    * Fix product function
    
    * Fix update Product
    
    * Fix addProduct function
    
    * Test Product Repo
    
    * Test addProduct function
    
    * Test addProduct
    
    * Fix addProduct function
    
    * Fix coupon discounts when type is 0
    
    * Add some product images and fix addProduct function
    
    * Add transaction limit and pessimistic lock
    
    * Update addProduct function
    
    Add product id added by seller to response
    
    * Fix addProduct function
    
    * Move some functions to another repo
    
    * 更新會員
    
    * 會員刪除
    
    * New DateTime
    
    * 新增會員
    
    * fix error
    
    * fix
    
    * fix error
    
    * Set permission
    
    * Fix permission
    
    * Fix permission
    
    * add seller
    
    * fix
    
    * fix error
    
    * test
    
    * test
    
    * test fix error
    
    * test
    
    * fix add seller error
    
    * test
    
    Co-authored-by: guyleaf <43506966+guyleaf@users.noreply.github.com>
    Co-authored-by: Lee <t107590039@ntut.org.tw>
    Co-authored-by: guyleaf <ychhua1@gmail.com>

--- app/Repositories/MemberRepository.php
+++ app/Repositories/MemberRepository.php
@@ -67,15 +67,15 @@ class MemberRepository
         ->delete();
     }
 
-    public function addCustomer($payload)
+    public function addMember($payload)
     {
         DB::beginTransaction();
 
         try
         {
-            $payload['member']['created_at'] = new DateTime('now', new DateTimeZone('Asia/Taipei'));
-            $payload['member']['created_at'] = $payload['member']['created_at']->format('Y-m-d H:i:s');
-            $payload['member']['updated_at'] = $payload['member']['created_at'];
+            $payload['created_at'] = new DateTime('now', new DateTimeZone('Asia/Taipei'));
+            $payload['created_at'] = $payload['created_at']->format('Y-m-d H:i:s');
+            $payload['updated_at'] = $payload['created_at'];
 
             $id = $this->memberTable
             ->orderByDesc('id')
@@ -83,11 +83,30 @@ class MemberRepository
             ->lockForUpdate()
             ->get(['id'])->first()->id + 1;
 
-            $payload['member']['id'] = $id;
-            $payload['customer']['member_id'] = $id;
+            $payload['id'] = $id;
 
             $this->memberTable
-            ->insert($payload['member']);
+            ->insert($payload);
+
+            return $id
+
+            DB::commit();
+        }
+        catch (Exception $e)
+        {
+            DB::rollBack();
+            throw $e;
+        }
+
+    }
+
+    public function addCustomer($payload, $member_id)
+    {
+        DB::beginTransaction();
+
+        try
+        {
+            $payload['member_id'] = $member_id;
 
             $this->customerTable
             ->insert($payload['customer']);
@@ -102,38 +121,24 @@ class MemberRepository
 
     }
 
-    public function addSeller($payload, $image_extension)
+    public function addSeller($payload, $member_id, $image_extension)
     {
         DB::beginTransaction();
 
         try
         {
-            $payload['member']['created_at'] = new DateTime('now', new DateTimeZone('Asia/Taipei'));
-            $payload['member']['created_at'] = $payload['member']['created_at']->format('Y-m-d H:i:s');
-            $payload['member']['updated_at'] = $payload['member']['created_at'];
-
-            $id = $this->memberTable
-            ->orderByDesc('id')
-            ->limit(1)
-            ->lockForUpdate()
-            ->get(['id'])->first()->id + 1;
-
             $counter_number = $this->sellerTable
             ->orderByDesc('counter_number')
             ->limit(1)
             ->lockForUpdate()
             ->get(['counter_number'])->first()->counter_number + 1;
 
-            $payload['member']['id'] = $id;
-            $payload['seller']['member_id'] = $id;
-            $payload['seller']['counter_number'] = $counter_number;
-            $payload['seller']['header_image'] = '/storage/restaurant/' . strval($id) . '/' . 'header' . '.' . $image_extension
-
-            $this->memberTable
-            ->insert($payload['member']);
+            $payload['member_id'] = $member_id;
+            $payload['counter_number'] = $counter_number;
+            $payload['header_image'] = '/storage/restaurant/' . strval($id) . '/' . 'header' . '.' . $image_extension
 
             $this->sellerTable
-            ->insert($payload['seller']);
+            ->insert($payload);
 
             return $id
 
