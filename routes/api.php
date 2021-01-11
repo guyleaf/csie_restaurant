4c32005dff5d7274aae64fad7e45dc07670888e3
guyleaf <ychhua1@gmail.com>
2021-01-11 19:04:28 +0800

    Move register for customer to AuthController

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'auth'], function () use ($router) {
     $router->post('login', 'AuthController@login');
+    $router->post('register', 'AuthController@register');
 
     $router->group(['middleware' => 'jwt.auth'], function () use ($router) {
         $router->post('logout', 'AuthController@logout');
