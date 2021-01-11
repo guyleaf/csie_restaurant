c8ec78fe00dd179037d683ebee0f1d8e27382ad3
Roger_NTUT_LAB1321 <asd30135931@gmail.com>
2021-01-12 00:00:04 +0800

    fix count

--- app/Repositories/MemberRepository.php
+++ app/Repositories/MemberRepository.php
@@ -62,12 +62,15 @@ class MemberRepository
 
         $sellers = $this->memberTable
             ->join('seller as S', 'S.member_id', '=', 'id')
+            ->where('is_deleted','=',false)
             ->orderBy('id')
             ->skip($currentNumber)
             ->take($requiredNumber)
             ->get(['id as seller_id', 'name', 'username', 'email', 'phone', 'member_status', 'counter_number']);
 
-        $numbers = $sellers->count();
+        $sellers = $this->memberTable
+            ->join('seller as S', 'S.member_id', '=', 'id')
+            ->where('is_deleted','=',false);
 
         return [$numbers, $sellers];
     }
