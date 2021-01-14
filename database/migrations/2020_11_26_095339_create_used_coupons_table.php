83a4e9096bbcc6d097a5dee259d33cf4a469a1e3
guyleaf <43506966+guyleaf@users.noreply.github.com>
2021-01-15 06:31:56 +0800

    Fixing..

--- database/migrations/2020_11_26_095339_create_used_coupons_table.php
+++ database/migrations/2020_11_26_095339_create_used_coupons_table.php
@@ -15,7 +15,7 @@ class CreateUsedCouponsTable extends Migration
     {
         Schema::create('used_coupon', function (Blueprint $table) {
             $table->foreignId("customer_id")->comment("所屬顧客編號");
-            $table->foreignId("coupon_code")->comment("優惠券代號");
+            $table->string("coupon_code")->comment("優惠券代號");
             $table->dateTime("usage_time")->comment("使用時間");
             $table->primary(["customer_id", "coupon_code"]);
             $table->foreign("customer_id")->references("member_id")->on("customer")
@@ -23,6 +23,7 @@ class CreateUsedCouponsTable extends Migration
             $table->foreign("coupon_code")->references("code")->on("coupon")
             ->onUpdate("cascade")->onDelete("cascade");
             $table->index("customer_id");
+            $table->index("coupon_code");
         });
     }
 
